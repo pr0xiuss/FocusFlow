c6c44e93f903c91dddff139731a591444cf74c35
Archit <archits21@gmail.com>
2025-11-01 08:09:52 +0000

    none

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',         
     'rest_framework_simplejwt', # JWT
     'corsheaders',            # CORS
-    'tasks', 
+    'task', 
     'focusflow',
     # 'django_elasticsearch_dsl',
 ]
